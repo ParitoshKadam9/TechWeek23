55667779aa3422cfbeba1c9d8c3c57084a9016dc
Paritosh Kadam <priyatamkadam@gmail.com>
2023-04-23 02:10:25 +0530

    Community box component on search map

--- af_frontend/src/components/CommunitiesBox.vue
+++ af_frontend/src/components/CommunitiesBox.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="CommBox">
-        
+        <div class="left">
+            <div class="CommName">{{communityName}}</div>
+        </div>
+        <div class="right">
+            <div class="CommID">#{{ communityID }}</div>
+        </div>
     </div>
 </template>
 <script>
@@ -8,18 +13,45 @@ export default {
     name: "CommuntiesBox",
     data() {
         return {
-                
+            communityName: "CS F407",
+            communityID: "4199"
             }
         }
     }
 </script>
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
     .CommBox{
         width: 100vh;
         margin-top: 2vh;
         min-height: 15vh;
         margin-left: auto;
         margin-right: auto;
-        background: #000;
+        background: #1e1e1e;
+        border-radius: 5px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        padding: 5vh;
+        cursor: pointer;
+        color: rgb(176, 176, 31);
+        transition: 0.1s;
+    }
+    .CommBox:hover{
+        background: rgb(176, 176, 31);
+        color: black;
+    }
+    .CommName{
+        font-size: 5vh;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  
+    }
+    .CommID{
+        font-size: 3vh;
+       font-family: 'Montserrat', sans-serif;  color: gray;
+        margin-right: 5vh;
+        
     }
 </style>
